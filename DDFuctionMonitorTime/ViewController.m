fd35221983c13e5172eb46aa09c8c12443b3b222
guohzong <guozhong@dangdang.com>
2020-04-07 17:32:51 +0800

    fix  bug

--- DDFuctionMonitorTime/ViewController.m
+++ DDFuctionMonitorTime/ViewController.m
@@ -42,14 +42,14 @@
     UIAlertController * alertVC = [UIAlertController alertControllerWithTitle:@"主线程中所有的方法耗时(误差0.1s)：" message:[resultString copy] preferredStyle:UIAlertControllerStyleAlert];
     UIAlertAction * confirmAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {}];
     [alertVC addAction:confirmAction];
-    
+    alertVC.modalPresentationStyle =  UIModalPresentationFullScreen;
     [self presentViewController:alertVC animated:nil completion:nil];
 }
 
 - (void)endBacktrace
 {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(DD_TIME_INTERVAL * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [DDFunctionMonitorTimeTool startMonitor];
+        [DDFunctionMonitorTimeTool stopMonitor];
     });
 }
 
